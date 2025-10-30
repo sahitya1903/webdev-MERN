2bca157a270389b2f7f0dedccf88b887422a05a6
Sahitya Kushwaha <83616018+sahitya1903@users.noreply.github.com>
2025-10-31 02:08:21 +0530

    Refactor JavaScript code for enhanced readability and functionality

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -4,3 +4,14 @@ console.log("Apna College");
 let a=5;
 let b=10;
 console.log("Sum is:",(a+b));
+
+
+let pencilPrice=10;
+let eraserPrice=6;
+// let output="Total Price:"+(pencilPrice+eraserPrice)+ " Rupees."
+let output=`Total Price: ${pencilPrice+eraserPrice} rupees.` //Template literal : $
+console.log(output);
+
+let c=10;
+console.log(c++); //10
+console.log(++c); //12
