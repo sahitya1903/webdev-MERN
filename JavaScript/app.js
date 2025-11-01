86cf6c169650f166e0aba448e1b42995d53de636
Sahitya Kushwaha <83616018+sahitya1903@users.noreply.github.com>
2025-11-01 15:54:48 +0530

    Implement string manipulation methods in JavaScript

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -90,6 +90,28 @@ console.warn("Warning Message");
 
 // alert("Danger");
 
-let fname= prompt("Enter your Name:");
-console.log(fname);
-alert(`Welcome, ${fname}!`);
+// let fname= prompt("Enter your Name:");
+// console.log(fname);
+// alert(`Welcome, ${fname}!`);
+
+
+let msg = "     Sahitya    ";
+let s = "IloveCoding";
+console.log(msg.trim());
+console.log(msg.toLowerCase());
+console.log(msg.toUpperCase());
+console.log(msg.trim().toUpperCase());
+console.log(s.indexOf("a"));
+console.log(s.indexOf("ov"));
+console.log(s.slice(5));
+console.log(s.slice(1, 5));
+console.log(s.slice(-5, -1));
+
+console.log(s.replace("o", "O"));
+console.log(s.replace("love", "LOVE"));
+console.log(s.repeat(2));
+
+
+let n = "ApnaCollege";
+let o= n.slice(4).replace("l", "t").replace("l", "t");
+console.log(o);
