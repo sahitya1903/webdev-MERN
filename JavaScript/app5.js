d71fe54fb1ff1a8aa11a0dc1eaff19664e3d6bd3
kushw <kushwahasahitya909@gmail.com>
2025-12-08 01:01:32 +0530

    Add createHelloWorld function and related usage example

--- JavaScript/app5.js
+++ JavaScript/app5.js
@@ -2,3 +2,19 @@ function hello() {
     console.log("Hello, World!");
 }
 hello();
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+console.log(createHelloWorld()());
