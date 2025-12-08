61cff5f70d4c0a6219a2134ce73af1d18907e6b1
kushw <kushwahasahitya909@gmail.com>
2025-12-09 01:48:35 +0530

    Add examples of anonymous functions, higher order functions, factory functions, and method shorthand in app6.js

--- JavaScript/app6.js
+++ JavaScript/app6.js
@@ -35,4 +35,84 @@ function outerFunc(){
 
 outerFunc();
 
+let product=function(a,b){      //Anonymous Function or Function Expression
+    return a*b;
+}
 
+console.log(product);
+console.log(product(2,5));
+
+product=function(a,b){      //Update function expression
+    return `Product: ${a*b}`;
+}
+
+console.log(product);
+console.log(product(2,5));
+
+
+function multipleGreet(func,n){     //Higher Order Function
+    for(let i=0;i<n;i++){
+        func();
+    }
+}
+
+let greet=function(){
+    console.log("hello");
+}
+
+multipleGreet(greet,2);
+
+function oddEvenTest(request){  //Factory function (Returns a function)
+    if(request=="odd"){
+        return function(n){
+            console.log(n%2!=0);
+        }
+    }else if(request=="even"){
+        return function(n){
+            console.log(n%2==0);
+        }
+    }else{
+        console.log("Wrong Request");
+    }
+}
+
+
+let func=oddEvenTest("odd");
+console.log(func);
+func(6);
+
+
+const calculator={          //Methos
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    },
+    mul: function(a,b){
+        return a*b;
+    }
+};
+
+console.log(calculator);
+console.log(calculator.add);
+console.log(calculator["add"]);
+console.log(calculator.add(2,3));
+
+
+const calculator2={     //Shortcut Way for writing method
+    add(a,b){
+        return a+b;
+    },
+    sub(a,b){
+        return a-b;
+    },
+    mul(a,b){
+        return a*b;
+    }
+};
+
+console.log(calculator2);
+console.log(calculator2.mul);
+console.log(calculator2["mul"]);
+console.log(calculator2.mul(2,3));
