73c452a925eb246e0eeff13f6a6064ef88c7c9ea
kushw <kushwahasahitya909@gmail.com>
2025-12-08 22:28:22 +0530

    Refactor and expand JavaScript functions: add utility functions, input handling, and code organization

--- JavaScript/app5.js
+++ JavaScript/app5.js
@@ -1,20 +1,82 @@
 function hello() {
     console.log("Hello, World!");
 }
-hello();
-/**
- * @return {Function}
- */
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World";
+
+
+function loop(){
+    for(let i=0;i<5;i++){
+        console.log(i);
     }
-};
+}
 
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
 
-console.log(createHelloWorld()());
+// let func =prompt("Enter function:");
+// if(func=="hello"){
+//     hello();
+// }else{
+//     loop();
+// }
+
+function dice(){
+    console.log(Math.floor(6*Math.random())+1);
+    //random number between 1 to 6
+}
+
+dice();
+
+function printName(name){
+    console.log(name);
+}
+
+printName("sahitya");
+
+function printInfo(name,age){
+    console.log(`Name: ${name}`);
+    console.log(`Age: ${age}`);
+}
+
+printInfo("Sahitya",22);
+printInfo("Mali");
+
+function sum(a,b){
+    return a+b;
+}
+
+let a=sum(2,3);
+console.log(a);
+
+function avg(a,b,c){
+    average=(a+b+c)/3;
+    console.log(average);
+}
+
+avg(1,2,3);
+
+console.log(avg);
+
+
+function table(n){
+    for(let i=1;i<11;i++){
+        console.log(`${n}*${i}=${n*i}`);
+    }
+}
+
+table(6);
+
+// //Leetcode Javascript Q1
+// /**
+//  * @return {Function}
+//  */
+// var createHelloWorld = function() {
+    
+//     return function(...args) {
+//         return "Hello World";
+//     }
+// };
+
+// /**
+//  * const f = createHelloWorld();
+//  * f(); // "Hello World"
+//  */
+
+// console.log(createHelloWorld()());
